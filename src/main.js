23c15852319f592b06937067eb43855597553dd0
Marc <marc.blanco.barcelo@gmail.com>
2025-12-14 10:53:10 +0100

    LEGOAT JAMES

--- src/main.js
+++ src/main.js
@@ -1,24 +1,75 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let background;
+let playerImage;
+let player = {}
 
-setupCounter(document.querySelector('#counter'))
+let camera = {
+  x: 0,
+  y: 0
+}
+
+function getKeys() {
+  let keys = {}
+  window.onkeydown = function (e) {
+      console.log(e.key);
+
+    keys[e.key] = true;
+  }
+  window.onkeyup = function (e) {
+    keys[e.key] = false;
+  }
+  
+  return keys;
+}
+
+let keys = getKeys();
+
+async function main() {
+  background = await loadImage("./img/cv.png");
+  playerImage = await loadImage("./img/placeholder.PNG");
+
+  requestAnimationFrame(mainLoop);
+
+}
+
+function loadImage(src) {
+    let img = new Image();
+    img.src = src;
+    return new Promise((resolve, reject) => {
+      img.onload = () => resolve(img);
+      img.onerror = reject;
+    })
+  }
+
+
+function mainLoop() {
+  update();
+  ctx.save();
+  draw();
+  ctx.restore()
+
+  requestAnimationFrame(mainLoop);
+}
+
+function draw() {
+  ctx.drawImage(playerImage, 0, 0);
+}
+
+function update() {
+  if (keys["ArrowUp"]) {
+    console.log("Lalelu");
+  }
+  if (keys["ArrowDown"]) {
+    console.log("Lalelu");
+  }
+  if (keys["ArrowLeft"]) {
+    console.log("Lalelu");
+  }
+  if (keys["ArrowRight"]) {
+    console.log("Lalelu");
+  }
+}
+
+main();
